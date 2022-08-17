88f6d619d339befa99cd9d902fd064dbc91c3791
aidana <dayseyedana@gmail.com>
2022-08-17 10:14:49 +0600

    Trial3 - Module3task2

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -2,11 +2,9 @@ package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
+    private int phoneNumber;
 
-        private int age;
-        private int phoneNumber;
-
-        public void callToFriend(int age) {
+        public void callToFriend(int ADULT_AGE) {
             //some code
         }
         public void callByNumber(int phoneNumber){
